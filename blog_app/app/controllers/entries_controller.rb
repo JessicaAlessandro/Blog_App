9767f52bc2033f84ddbbaea7166fd16ebd1f16ec
Jessica Alessandro <JessicaAlessandro@Jessicas-MacBook-Pro.local>
2015-11-05 16:00:05 -0500

    request actions working for entries controller. completed views for entry: show, new, edit, and. new and edit use the form

--- blog_app/app/controllers/entries_controller.rb
+++ blog_app/app/controllers/entries_controller.rb
@@ -3,4 +3,44 @@ class EntriesController < ApplicationController
 	def index 
     @entries = Entry.all
 	end
+
+	def new
+		@entry = Entry.new
+	end
+
+  def create
+    @entry = Entry.create(entry_params)
+    redirect_to entries_path
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+		if @entry.update(entry_params)
+			redirect_to entries_path
+		else
+			render :action => :edit
+		end
+	end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
+  end
 end
+
+ private
+
+  def entry_params
+    params.require(:entry).permit(:title, :author, :content, :created_at)
+  end
+
+
