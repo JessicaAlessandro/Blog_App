d5d3e0b0c4ccacc2b082d26efb368298a3171ef7
Jessica Alessandro <JessicaAlessandro@Jessicas-MacBook-Pro.local>
2015-11-06 21:38:41 -0500

    ran migration for comments, and added association to enrty model

--- blog_app/db/schema.rb
+++ blog_app/db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105220719) do
+ActiveRecord::Schema.define(version: 20151107023521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "entry_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["entry_id"], name: "index_comments_on_entry_id", using: :btree
 
   create_table "entries", force: :cascade do |t|
     t.string   "title"
@@ -43,4 +53,5 @@ ActiveRecord::Schema.define(version: 20151105220719) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "entries"
 end
