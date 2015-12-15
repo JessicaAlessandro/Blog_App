616c18a6c4d8e2dad691c624bdac72e495394b83
Jessica Alessandro <JessicaAlessandro@Jessicas-MacBook-Pro.local>
2015-12-15 15:16:36 -0500

    made css responsive

--- db/migrate/20151105193420_create_entries.rb
+++ db/migrate/20151105193420_create_entries.rb
@@ -8,3 +8,16 @@ class CreateEntries < ActiveRecord::Migration
     end
   end
 end
+
+
+
+    	  t.string   :title
+        t.string   :arist
+        t.text     :year
+        t.string   :time_period
+        t.text     :medium
+    	  t.string   :provenance
+        t.string   :repatriared
+        t.text     :description
+        t.string   :current_location
+        t.text     :img_url
